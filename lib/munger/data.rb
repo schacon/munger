40fc6e67399c45d81c23c51330a43b9b195e5906
Brandon Mitchell <brandon@systemisdown.net>
2008-04-26 15:20:10 -0700

    Object#respond_to should be #respond_to?

--- lib/munger/data.rb
+++ lib/munger/data.rb
@@ -126,8 +126,8 @@ module Munger
     
     def valid?
       if ((@data.size > 0) &&
-        (@data.respond_to :each_with_index) &&
-        (@data.first.respond_to :keys)) &&
+        (@data.respond_to? :each_with_index) &&
+        (@data.first.respond_to? :keys)) &&
         (!@data.first.is_a? String)
         return true
       else
